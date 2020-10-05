527fcb02005f321252980f53f65280ffa78b75d1
Gina <ginacdlr@gmail.com>
2020-10-04 18:43:54 -0700

    Removed storyboard and organized files for MVC

--- SpookyPexels/Controller/MainController.swift
+++ SpookyPexels/Controller/MainController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainController.swift
 //  SpookyPexels
 //
 //  Created by Gina De La Rosa on 10/4/20.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 

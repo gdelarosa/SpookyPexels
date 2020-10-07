c4e240364d783849cee47d763aee7e043af32d12
Gina <ginacdlr@gmail.com>
2020-10-06 19:19:42 -0700

    Start of networking setup

---
+++ SpookyPexels/Networking/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  SpookyPexels
+//
+//  Created by Gina De La Rosa on 10/6/20.
+//
+
+import Foundation
+
+struct API {
+    static let PHOTO_BASE_URL = "https://api.pexels.com/v1"
+    static let VIDEO_BASE_URL = "https://api.pexels.com/videos"
+    static let PHOTO_SEARCH_URL = "https://api.pexels.com/v1/search"
+    static let VIDEO_SEARCH_URL = "https://api.pexels.com/videos/search"
+    static let PHOTO_HALLOWEEN_URL = "https://api.pexels.com/v1/search?query=halloween&per_page=1"
+    static let VIDEO_HALLOWEEN_URL = "https://api.pexels.com/videos/search?query=halloween&per_page=1"
+    static let API_KEY = "" 
+    
+    
+}
---
+++ SpookyPexels/Networking/Service.swift
@@ -0,0 +1,12 @@
+//
+//  Service.swift
+//  SpookyPexels
+//
+//  Created by Gina De La Rosa on 10/6/20.
+//
+
+import Foundation
+
+class Service {
+    
+}

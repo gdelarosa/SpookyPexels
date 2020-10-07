1b83a3373855d593e2193aa9ad054f661b6c421d
Gina <ginacdlr@gmail.com>
2020-10-06 18:53:17 -0700

    Added error handling

---
+++ SpookyPexels/ErrorHandling/ErrorHandling.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorHandling.swift
+//  SpookyPexels
+//
+//  Created by Gina De La Rosa on 10/6/20.
+//
+
+import Foundation
+
+enum ErrorHandling: Error {
+    case noInternet
+    case invalidRequest
+    case requestFailed
+    case unknown
+    
+}
